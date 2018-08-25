03aab91fc2eba651eae1dce2a2c6f60d1c04cb99
wanjx <wjx2699@outlook.com>
2018-08-25 19:34:58 +0800

    add server

---
+++ ch1/server2/server2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"sync"
+	"log"
+	"net/http"
+	"fmt"
+)
+
+/**
+* Created by wanjx on 2018/8/25.
+*/
+
+var count int
+var mu sync.Mutex
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+
