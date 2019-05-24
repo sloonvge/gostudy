373e7aa6e02e22df4918278b596c466ec63783c6
wanjx <wjx2699@outlook.com>
2019-05-24 23:32:32 +0800

    !

--- gopl/src/ch7/sorting.go
+++ gopl/src/ch7/sorting.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+	"time"
+)
+
 /** 
 * Created by wanjx in 2019/5/13 23:20
 **/
  
+type Track struct {
+	Title string
+	Artist string
+	Album string
+	Year int
+	Length time.Duration
+}
+
+
+func length(s string) time.Duration{
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+func printTracks(tracks []*Track)  {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8,
+		2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush()
+}
+
+type byArtist []*Track
+
+func (t byArtist) Len() int { return len(t) }
+func (t byArtist) Less(i, j int) bool { return t[i].Artist < t[j].Artist }
+func (t byArtist) Swap(i, j int) { t[i], t[j] = t[j], t[i]}
--- gopl/src/ch7/sorting_test.go
+++ gopl/src/ch7/sorting_test.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"sort"
+	"testing"
+)
+
 /** 
 * Created by wanjx in 2019/5/13 23:31
 **/
- 
+
+var track = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func TestByArtist(t *testing.T) {
+	sort.Sort(byArtist(track))
+
+	printTracks(track)
+}
