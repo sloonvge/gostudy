c44d9bccf39bcb146ec18ca06d6cf807efdc3b88
wanjx <wjx2699@outlook.com>
2019-08-13 12:58:32 +0800

    ...

--- gotp/src/klog/go.mod
+++ gotp/src/klog/go.mod
@@ -1,10 +1,9 @@
 module klog
 
-go 1.12
+go 1.11
 
 require (
-	github.com/Shopify/sarama v1.23.1 // indirect
-	github.com/hpcloud/tail v1.0.0 // indirect
-	gopkg.in/fsnotify.v1 v1.4.7 // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	github.com/DataDog/zstd v1.4.1 // indirect
+	github.com/Shopify/sarama v1.23.1
+	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
 )
--- gotp/src/klog/src/day11/kafka/main.go
+++ gotp/src/klog/src/day11/kafka/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Shopify/sarama"
+	"log"
 )
 
 func main() {
 	config := sarama.NewConfig()
-	
+
+	client, err := sarama.NewSyncProducer([]string{"192.168.14.7:9092"}, config)
+	if err != nil {
+		log.Println("producer close, err: ", err)
+		return
+	}
+	defer client.Close()
+
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "nginx_log"
+	msg.Value = sarama.StringEncoder("this is a good test, my message is good")
+
+	pid, offset, err := client.SendMessage(msg)
+	if err != nil {
+		log.Println("send message fail, err: ", err)
+		return
+	}
+
+	log.Printf("pid:%v, offset:%v\n", pid, offset)
 }
